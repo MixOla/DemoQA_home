d4ad7033e7f95e1fe594540dbd06dd16c1ded036
@omi-coder <omi-ufa@yandex.ru>
2025-04-08 15:44:04 +0300

    Added tests

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -4,6 +4,25 @@ from pages.swag_labs import BasePage
 
 
 def test_icon_exist(browser):
-    suacedomo_page = BasePage(browser)
+    suacedomo_page = SwagLabs(browser)
     suacedomo_page.visit()
-    assert suacedomo_page.find_element(locator='#login-button')
+    assert suacedomo_page.exist_icon() is True, "Иконка не найдена"
+
+
+def test_icon_exist(browser):
+    "Проверяет наличие иконки"
+    suacedomo_page = SwagLabs(browser)
+    suacedomo_page.visit()
+    assert suacedomo_page.exist_icon() is True, "Иконка не найдена"
+
+def test_icon_username_exist(browser):
+    "Проверяет наличие поля имени"
+    suacedomo_page = SwagLabs(browser)
+    suacedomo_page.visit()
+    assert suacedomo_page.exist_username() is True, "Поле имени не найдено"
+
+def test_icon_password_exist(browser):
+    "Проверяет наличие поля пароля"
+    suacedomo_page = SwagLabs(browser)
+    suacedomo_page.visit()
+    assert suacedomo_page.exist_password() is True, "Поле пароля не найдено"
