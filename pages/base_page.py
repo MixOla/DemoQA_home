4e27a1d42912623366ac8c45b03cd1c8a59150e5
@omi-coder <omi-ufa@yandex.ru>
2025-04-08 15:40:41 +0300

    Added BasePage and methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
@@ -10,4 +11,3 @@ class BasePage:
 
     def find_element(self, locator):
         return self.driver.find_element(By.CSS_SELECTOR, locator)
-
