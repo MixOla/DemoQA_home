90979ef870eb6ad2daddfc856643413ae0cd77c8
@omi-coder <omi-ufa@yandex.ru>
2025-04-08 15:42:19 +0300

    Added SwagLabs and methods

--- pages/swag_labs.py
+++ pages/swag_labs.py
@@ -3,15 +3,27 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class SwagLabs(BasePage):
-    pass
 
-    # def exist_icon(self):
-    #     try:
-    #         self.find_element(locator='div.login_logo')
-    #     except NoSuchElementException:
-    #         return False
-    #
-    #     return True
-    #
-    #
-    #
+    def exist_icon(self):
+        try:
+            self.find_element(locator='div.login_logo')
+        except NoSuchElementException:
+            return False
+
+        return True
+
+    def exist_username(self):
+        try:
+            self.find_element(locator='#user-name')
+        except NoSuchElementException:
+            return False
+
+        return True
+
+    def exist_password(self):
+        try:
+            self.find_element(locator='#password')
+        except NoSuchElementException:
+            return False
+
+        return True
