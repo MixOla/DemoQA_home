075d215a0d65606048d99d041363f7a779685435
@omi-coder <omi-ufa@yandex.ru>
2025-04-08 15:45:16 +0300

    Added tests

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -4,22 +4,18 @@ from pages.swag_labs import BasePage
 
 
 def test_icon_exist(browser):
-    suacedomo_page = SwagLabs(browser)
-    suacedomo_page.visit()
-    assert suacedomo_page.exist_icon() is True, "Иконка не найдена"
-
-
-def test_icon_exist(browser):
     "Проверяет наличие иконки"
     suacedomo_page = SwagLabs(browser)
     suacedomo_page.visit()
     assert suacedomo_page.exist_icon() is True, "Иконка не найдена"
+
 
 def test_icon_username_exist(browser):
     "Проверяет наличие поля имени"
     suacedomo_page = SwagLabs(browser)
     suacedomo_page.visit()
     assert suacedomo_page.exist_username() is True, "Поле имени не найдено"
+
 
 def test_icon_password_exist(browser):
     "Проверяет наличие поля пароля"
